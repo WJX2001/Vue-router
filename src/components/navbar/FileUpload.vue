310fea572f32439e5f7ede00357e5084da60d7cb
WJX2001 <1758807220@qq.com>
2023-02-08 17:40:06 +0800

    文件序列化& 并行传输

--- src/components/navbar/FileUpload.vue
+++ src/components/navbar/FileUpload.vue
@@ -17,7 +17,7 @@
       type="file"
       @change="getMulFile($event)"
     />
-    <el-button @click="uploadMulti">点击上传{{ fileList.length }}个文件</el-button>
+    <el-button @click="uploadMultiSeq">点击上传{{ fileList.length }}个文件</el-button>
 
     <div>
       <el-carousel  v-show="urls.length!==0">
@@ -47,15 +47,62 @@ export default {
     };
   },
   methods: {
+    // 并行化传输   两个请求没有依赖关系时，应该尽可能快的发起
+    async uploadMultiTogether() {
+      // 没办法直到哪个请求最后完成
+      // this.fileList.forEach(async (file) => {
+      //   const data = new FormData()
+      //   data.append('file',file)
+      //   const {data: result} = await http.post("/upload",  /**放file文件 */ data);
+      //   this.urls.push(result.data.netUrl)
+      // })
+      // promise.all
+      
+        const asyncTask = []
+        this.fileList.forEach((file) => {
+            const data = new FormData()
+            data.append('file',file)
 
+            // 此时的task就是一个promise,如果想取到后面的结果需要 .then
+            // 而await是通过语法糖的形式直接拿到后面的参数 赋值给前面的变量
+            const task = http.post("/upload",  /**放file文件 */ data)
+            asyncTask.push(task)
+        })
+
+        // 并发传输由于找不到适合的时机 找到一个同时结束的时间点，所以用Promise.all
+        const result = await Promise.all(asyncTask)
+        console.log(result)
+        // 每一个arr对应着 http的post请求后的task的返回值
+        // Promise.all(asyncTask).then((arr) => {
+
+        // })
+          
+
+
+      // 同时拿到两个netUrl
+    },
+
+    // 序列化上传
+    async uploadMultiSeq() {
+      for(let i=0;i<this.fileList.length;i++) {
+        const data = new FormData()
+        data.append('file',this.fileList[i])
+        const {data:result} = await http.post("/upload",  /**放file文件 */ data);
+        this.urls.push(result.data.netUrl)
+        console.log(result)
+      }
+    },
+    
+    // 多文件一次性上传
     async uploadMulti() {
         const allData = new FormData()
         this.fileList.forEach((file,index) => {
             allData.append(`multi${index}`,file)
         })
-        // 一次性上传
+        
         const { data: result } = await http.post("/upload/multi",  /**放file文件 */ allData);
         this.urls = result.data.urls
+        console.log(result)
     },
 
     openMulti() {
